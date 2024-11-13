136eadfa76505e16bdc69e8af3f4f42951de648a
Thorns-H <abrahamm9986@gmail.com>
2024-11-13 10:04:54 -0600

    feat: added information display with simple flask frontend

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+with open('output.json') as file:
+    matches = json.load(file)
+
+@app.route('/')
+def index():
+    return render_template('index.html', matches=matches)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
